5a223df9d1faeb4d8a7c75f26ef2ea85129d5ca2
ChungJooEun <nina2439@naver.com>
2021-12-21 15:21:52 +0900

    initial commit

---
+++ public/assets/js/modal.js
@@ -0,0 +1,11 @@
+$(function(){ 
+
+    $("button.modal-btn").click(function(){
+      $(".modal").fadeIn();
+    });
+    
+    $("#modal-btn").click(function(){
+      $(".modal").fadeOut();
+    });
+    
+  })
