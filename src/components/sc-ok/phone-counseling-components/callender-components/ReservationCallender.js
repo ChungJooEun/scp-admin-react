a5d6588f93a4e6fe95bad48670cde80ea2f829dc
ChungJooEun <nina2439@naver.com>
2021-12-28 16:26:00 +0900

    서초OK 생활 자문단 > 전화상담 - 일정 조회 캘린더 구현

--- src/components/sc-ok/phone-counseling-components/callender-components/ReservationCallender.js
+++ src/components/sc-ok/phone-counseling-components/callender-components/ReservationCallender.js
@@ -1,58 +1,165 @@
-import React from "react";
-
-import Paging from "../../../common-components/Paging";
+import React, { useState, useEffect, useRef } from "react";
 
 import DayComponent from "./DayComponent";
 import ReservatableSchedule from "./ReservatableSchedule";
 import ReservedSchedule from "./ReservedSchedule";
 
+const createYearOption = () => {
+  let yearOption = [];
+
+  for (let i = 2021; i < 2042; i++) {
+    yearOption.push(
+      <option value={i} key={i}>
+        {i}년
+      </option>
+    );
+  }
+
+  return yearOption;
+};
+
+const createMonthOption = () => {
+  let monthOption = [];
+
+  for (let i = 1; i <= 12; i++) {
+    monthOption.push(
+      <option value={i} key={i}>
+        {i}월
+      </option>
+    );
+  }
+
+  return monthOption;
+};
+
 const ReservationCallender = () => {
+  const [date, setDate] = useState(null);
+
+  const yearOption = useRef();
+  const monthOption = useRef();
+
+  const searchSchedule = (e) => {
+    e.preventDefault();
+
+    setDate({
+      year: yearOption.current.value,
+      month: monthOption.current.value,
+    });
+  };
+
+  const setCallenderRows = (
+    startDate,
+    isFisrtRows,
+    firstDateofDay,
+    lastDate
+  ) => {
+    let callender = [];
+    let endDate = startDate + 6;
+
+    for (let i = startDate; i <= endDate; i++) {
+      if (isFisrtRows) {
+        if (i - 1 < firstDateofDay) {
+          callender.push(<td key={i}></td>);
+        } else {
+          callender.push(
+            <td key={i}>
+              <DayComponent day={i - firstDateofDay} />
+              <ReservatableSchedule />
+              <ReservedSchedule />
+            </td>
+          );
+        }
+      } else {
+        if (i <= lastDate) {
+          callender.push(
+            <td key={i}>
+              <DayComponent day={i} />
+              <ReservatableSchedule />
+              <ReservedSchedule />
+            </td>
+          );
+        } else {
+          callender.push(<td key={i}></td>);
+        }
+      }
+    }
+
+    return callender;
+  };
+
+  const setCallender = () => {
+    // 0 : 일 ~ 6 : 토
+    const firstDateofDay = new Date(date.year, date.month - 1, 1).getDay();
+    const lastDate = new Date(date.year, date.month, 0).getDate();
+
+    let callenderAry = [];
+
+    let callenderRows = Math.ceil((lastDate - (7 - firstDateofDay)) / 7);
+
+    let callenderDate = 1;
+
+    for (let i = 0; i <= callenderRows; i++) {
+      callenderAry.push(
+        <tr key={i}>
+          {setCallenderRows(
+            callenderDate,
+            i === 0 ? true : false,
+            firstDateofDay,
+            lastDate
+          )}
+        </tr>
+      );
+
+      i === 0 ? (callenderDate += 7 - firstDateofDay) : (callenderDate += 7);
+    }
+
+    return callenderAry;
+  };
+
+  useEffect(() => {
+    const today = new Date();
+
+    setDate({
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+    });
+  }, []);
+
+  if (!date) {
+    return <div></div>;
+  }
+
   return (
     <>
       <div className="page-separator mt-10">
-        <div className="page-separator__text">2021년 1월</div>
+        <div className="page-separator__text">
+          {date.year}년 {date.month}월
+        </div>
       </div>
       <div className="card mb-24pt">
         <div className="card-header">
-          <form className="form-inline">
+          <form className="form-inline" onSubmit={searchSchedule}>
             <select
               id="inlineFormRole"
               className="custom-select mb-2 mr-sm-2 mb-sm-0"
+              defaultValue={date.year}
+              name="year"
+              ref={yearOption}
             >
-              <option value="All Roles">2021년</option>
-              <option value="All Roles">2022년</option>
-              <option value="All Roles">2023년</option>
-              <option value="All Roles">2024년</option>
-              <option value="All Roles">2025년</option>
-              <option value="All Roles">2026년</option>
-              <option value="All Roles">2027년</option>
-              <option value="All Roles">2028년</option>
-              <option value="All Roles">2029년</option>
-              <option value="All Roles">2031년</option>
-              <option value="All Roles">2032년</option>
-              <option value="All Roles">2033년</option>
-              <option value="All Roles">2034년</option>
-              <option value="All Roles">2035년</option>
-              <option value="All Roles">2036년</option>
-              <option value="All Roles">2037년</option>
+              {createYearOption()}
             </select>
             <select
               id="inlineFormRole"
               className="custom-select mb-2 mr-sm-2 mb-sm-0"
+              name="month"
+              defaultValue={date.month}
+              ref={monthOption}
             >
-              <option value="All Roles">1월</option>
-              <option value="All Roles">2월</option>
-              <option value="All Roles">3월</option>
-              <option value="All Roles">4월</option>
-              <option value="All Roles">5월</option>
-              <option value="All Roles">6월</option>
-              <option value="All Roles">7월</option>
-              <option value="All Roles">8월</option>
-              <option value="All Roles">9월</option>
-              <option value="All Roles">10월</option>
-              <option value="All Roles">11월</option>
-              <option value="All Roles">12월</option>
+              {createMonthOption()}
             </select>
+            <button className="btn btn-primary" type="submit">
+              검색
+            </button>
           </form>
         </div>
         <div
@@ -63,6 +170,11 @@ const ReservationCallender = () => {
           <table className="table table-bordered table-flush mb-0 thead-border-top-0 table-nowrap">
             <thead>
               <tr>
+                <th>
+                  <div className="lh-1 d-flex flex-column text-50 my-4pt">
+                    일
+                  </div>
+                </th>
                 <th>
                   <div className="lh-1 d-flex flex-column text-50 my-4pt">
                     월
@@ -93,161 +205,15 @@ const ReservationCallender = () => {
                     토
                   </div>
                 </th>
-                <th>
-                  <div className="lh-1 d-flex flex-column text-50 my-4pt">
-                    일
-                  </div>
-                </th>
               </tr>
             </thead>
             <tbody className="list" id="contacts calendar">
-              <tr>
-                <td>
-                  <DayComponent day="1" />
-                  <ReservatableSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="2" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-
-                <td>
-                  <DayComponent day="3" />
-                  <ReservatableSchedule />
-                </td>
-                <td>
-                  <DayComponent day="4" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="5" />
-                  <ReservedSchedule />
-                  <ReservatableSchedule />
-                </td>
-                <td>
-                  <DayComponent day="6" />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="7" />
-                  <ReservedSchedule />
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <DayComponent day="8" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="9" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="10" />
-                  <ReservedSchedule />
-                  <ReservatableSchedule />
-                </td>
-                <td>
-                  <DayComponent day="11" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="12" />
-                  <ReservatableSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="13" />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="14" />
-                  <ReservedSchedule />
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <DayComponent day="15" />
-                  <ReservedSchedule />
-                  <ReservatableSchedule />
-                </td>
-                <td>
-                  <DayComponent day="16" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="17" />
-                  <ReservatableSchedule />
-                  <ReservatableSchedule />
-                </td>
-                <td>
-                  <DayComponent day="18" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="19" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="20" />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="21" />
-                  <ReservatableSchedule />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <DayComponent day="22" />
-                  <ReservatableSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="23" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="24" />
-                  <ReservatableSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="25" />
-                  <ReservedSchedule />
-                  <ReservatableSchedule />
-                </td>
-                <td>
-                  <DayComponent day="26" />
-                  <ReservedSchedule />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="27" />
-                  <ReservedSchedule />
-                </td>
-                <td>
-                  <DayComponent day="28" />
-                  <ReservatableSchedule />
-                </td>
-              </tr>
+              {setCallender()}
             </tbody>
           </table>
         </div>
 
-        <Paging />
+        {/* <Paging /> */}
       </div>
 
       <br />
