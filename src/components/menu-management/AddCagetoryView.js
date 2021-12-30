c4c19d7af1d20efe3d771b17ffb76ac73eaa6eed
ChungJooEun <nina2439@naver.com>
2021-12-30 12:09:15 +0900

    메뉴 관리 > 카테고리 관리 - 카테고리 추가 기능 구현

--- src/components/menu-management/AddCagetoryView.js
+++ src/components/menu-management/AddCagetoryView.js
@@ -1,20 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
 import CategoryForm from "./category-management-components/CategoryForm";
 import CategoryImage from "./category-management-components/CategoryImage";
 
+const useConfirm = (message = null, onConfirm) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    }
+  };
+  return confirmAction;
+};
+
 const AddCategoryView = () => {
+  const history = useHistory();
+
+  const [categoryInfo, setCategoryInfo] = useState({
+    name: "",
+    img: `${process.env.PUBLIC_URL}/assets/images/stories/256_rsz_thomas-russell-751613-unsplash.jpg`,
+  });
+
+  // 카테고리명 수정
+  const getCategoryName = (editName) => {
+    setCategoryInfo({
+      ...categoryInfo,
+      name: editName,
+    });
+  };
+
+  // 카테고리 이미지 수정
+  const getCategoryImg = (imgFile) => {
+    setCategoryInfo({
+      ...categoryInfo,
+      img: imgFile,
+    });
+  };
+
+  // 카테고리 수정 axios 요청
+  const requestSaveCategory = async (data) => {
+    const url = "http://118.67.153.236:8080/api/v1/menu/category";
+
+    try {
+      const response = await axios.post(url, data);
+
+      if (response.status === 200) {
+        alert("저장되었습니다.");
+        history.push("/main-menu/category");
+      }
+    } catch (e) {
+      alert("저장에 실패하였습니다.");
+      console.log(e);
+    }
+  };
+
+  // 확인 버튼 클릭시, 수행
+  const onHandleSaveCategory = () => {
+    let formData = new FormData();
+
+    if (typeof categoryInfo.img === "object") {
+      formData.append("file", categoryInfo.img[0]); // 이미지
+    }
+
+    formData.append("name", categoryInfo.name); // 카테고리 명
+
+    for (let v of formData.values()) {
+      console.log(v);
+    }
+
+    // requestSaveCategory(formData);
+  };
+
+  // 카테고리 수정 내용 저장
+  const onClickSaveBtn = useConfirm(
+    "새로운 카테고리를 저장하시겠습니까?",
+    onHandleSaveCategory
+  );
+
   return (
     <>
-      {/* <div className="preloader">
-        <div className="sk-chase">
-          <div className="sk-chase-dot"></div>
-          <div className="sk-chase-dot"></div>
-          <div className="sk-chase-dot"></div>
-          <div className="sk-chase-dot"></div>
-          <div className="sk-chase-dot"></div>
-          <div className="sk-chase-dot"></div>
-        </div>
-      </div> */}
       <div
         className="mdk-drawer-layout js-mdk-drawer-layout"
         data-push
@@ -30,14 +99,22 @@ const AddCategoryView = () => {
                 <div className="col-lg-4">
                   <div className="card mb-lg-0">
                     <div className="list-group list-group-flush">
-                      <CategoryImage />
+                      <CategoryImage
+                        imgSrc={categoryInfo.img}
+                        getCategoryImg={getCategoryImg}
+                      />
                     </div>
                   </div>
                 </div>
                 <div className="col-lg-8">
                   <div className="card mb-lg-0">
                     <div className="list-group list-group-flush">
-                      <CategoryForm type="add" />
+                      <CategoryForm
+                        type="add"
+                        categoryName={categoryInfo.name}
+                        getCategoryName={getCategoryName}
+                        onClickSaveBtn={onClickSaveBtn}
+                      />
                     </div>
                   </div>
                 </div>
