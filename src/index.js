7b2479d4848a8a52d9556e4826b1957ccebe5fb5
ChungJooEun <nina2439@naver.com>
2022-01-05 11:23:42 +0900

    react 설정 업데이트 - react.retrictMode 추가

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
+
+reportWebVitals();
