992bda651872e209d51faaff968234ab4811b38a
ChungJooEun <nina2439@naver.com>
2022-01-11 20:09:49 +0900

    게시판 관리 > 개인정보 처리방침

--- src/components/community/PersnalInformationPolicyView.js
+++ src/components/community/PersnalInformationPolicyView.js
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MenuContext from "../../context/menu";
+import axios from "axios";
 
 import BottomSaveBtn from "../common-components/BottomSaveBtn";
 import Editor from "../common-components/editor-components/Editor";
@@ -14,10 +15,112 @@ const pagePathList = [
   },
 ];
 
+const useConfirm = (message = null, onConfirm) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    }
+  };
+  return confirmAction;
+};
+
 const PersnalInformationPolicyView = () => {
   const { state, actions } = useContext(MenuContext);
 
+  const [moreInformation, setMoreInformation] = useState("");
+  const [communityState, setCommunityState] = useState(null);
+
+  const [isFirst, setIsFirst] = useState(null);
+
+  const onChangeMoreInformation = (data) => {
+    setMoreInformation(data);
+  };
+
+  const onChangeCommunityState = (data) => {
+    setCommunityState(data);
+  };
+
+  const onHandleSaveBtn = () => {
+    let data = new Object();
+
+    data.moreInformation = moreInformation;
+    data.state = communityState;
+    data.userId = sessionStorage.getItem("userId");
+    data.type = "PRIVACY";
+
+    isFirst ? addPrivacyPolicy(data) : editPrivacyPolicy(data);
+  };
+
+  const addPrivacyPolicy = async (data) => {
+    const url = `http://${process.env.REACT_APP_SERVICE_IP}:${process.env.REACT_APP_UPLOAD_SERVICE_PORT}/api/upload/community/create`;
+
+    try {
+      const response = await axios.post(url, data);
+
+      if (response.status === 201) {
+        alert("개인정보 처리방침이 저장되었습니다.");
+      }
+    } catch (e) {
+      alert("개인정보 처리방침 저장에 실패하였습니다.");
+      console.log(e);
+    }
+  };
+
+  const editPrivacyPolicy = async (data) => {
+    const url = `http://${process.env.REACT_APP_SERVICE_IP}:${process.env.REACT_APP_UPLOAD_SERVICE_PORT}/api/upload/community/update`;
+
+    try {
+      const response = await axios.post(url, data);
+
+      if (response.status === 201) {
+        alert("개인정보 처리방침이 수정되었습니다.");
+      }
+    } catch (e) {
+      alert("개인정보 처리방침 수정에 실패하였습니다.");
+      console.log(e);
+    }
+  };
+
+  const onClickSaveBtn = useConfirm(
+    `개인정보 처리방침을 ${
+      {
+        TEMP_SAVE: "'임시저장'",
+        POST: "'게시'",
+      }[communityState]
+    } 상태로 저장하시겠습니까?`,
+    onHandleSaveBtn
+  );
+
   useEffect(() => {
+    const getPrivacyPolicyInfo = async () => {
+      const url = `http://${process.env.REACT_APP_SERVICE_IP}:${process.env.REACT_APP_SERVICE_PORT}/api/v1/community/privacy`;
+
+      try {
+        const response = await axios.get(url);
+
+        if (response.status === 200) {
+          setIsFirst(Object.keys(response.data).includes("id") ? false : true);
+
+          setMoreInformation(
+            Object.keys(response.data).includes("moreInformation")
+              ? response.data.moreInformation
+              : ""
+          );
+
+          setCommunityState(response.data.state);
+        }
+      } catch (e) {
+        alert("개인정보 처리방침 상세조회 중, 오류가 발생하였습니다.");
+        console.log(e);
+      }
+    };
+
+    getPrivacyPolicyInfo();
+
     if (state.menu.topMenu !== 5 || state.menu.subMenu !== 5) {
       actions.setMenu({
         topMenu: 5,
@@ -61,49 +164,41 @@ const PersnalInformationPolicyView = () => {
   }, []);
 
   return (
-    <>
-      {/* <div className="preloader">
-        <div className="sk-chase">
-            <div className="sk-chase-dot">
-            </div>
-            <div className="sk-chase-dot">
-            </div>
-            <div className="sk-chase-dot">
-            </div>
-            <div className="sk-chase-dot">
-            </div>
-            <div className="sk-chase-dot">
-            </div>
-            <div className="sk-chase-dot">
-            </div>
-        </div>
-    </div> */}
-      <div
-        className="mdk-drawer-layout js-mdk-drawer-layout"
-        data-push
-        data-responsive-width="992px"
-      >
-        <div className="mdk-drawer-layout__content page-content">
-          <GlobalBar />
-          <PageTitle
-            pageTitle="개인정보 처리방침"
-            pagePathList={pagePathList}
-            onlyTitle={true}
-          />
+    <div
+      className="mdk-drawer-layout js-mdk-drawer-layout"
+      data-push
+      data-responsive-width="992px"
+    >
+      <div className="mdk-drawer-layout__content page-content">
+        <GlobalBar />
+        <PageTitle
+          pageTitle="개인정보 처리방침"
+          pagePathList={pagePathList}
+          onlyTitle={true}
+        />
 
-          <div className="container-fluid page__container">
-            <div className="page-section">
-              <div className="page-separator">
-                <div className="page-separator__text">상세정보</div>
-              </div>
-              <Editor />
+        <div className="container-fluid page__container">
+          <div className="page-section">
+            <div className="page-separator">
+              <div className="page-separator__text">상세정보</div>
             </div>
-            <BottomSaveBtn type="detail" />
+            <Editor
+              moreInformation={moreInformation}
+              onChangeMoreInformation={onChangeMoreInformation}
+            />
           </div>
+          {communityState && (
+            <BottomSaveBtn
+              type="add"
+              onClickSaveBtn={onClickSaveBtn}
+              state={communityState}
+              onChangeState={onChangeCommunityState}
+            />
+          )}
         </div>
-        <SideMenuBar />
       </div>
-    </>
+      <SideMenuBar />
+    </div>
   );
 };
 
