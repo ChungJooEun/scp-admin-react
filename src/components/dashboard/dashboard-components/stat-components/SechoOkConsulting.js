d68df0c7c62bbbd46b3574a3527eb66067817599
ChungJooEun <nina2439@naver.com>
2022-02-17 10:41:39 +0900

    대시보드 - 통계 - 서초 Ok 생활 온라인/전화 상담 통계

--- src/components/dashboard/dashboard-components/stat-components/SechoOkConsulting.js
+++ src/components/dashboard/dashboard-components/stat-components/SechoOkConsulting.js
@@ -1,171 +1,130 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { calcRate } from "../../../../util/char-options";
+import { parsingMonthDate } from "../../../../util/date-convert-function";
 
-const SeochoOkConsulting = () => {
+import HorizontalBarGraphComponent from "./HorizontalBarGraphComponent";
+import LineGraphComponent from "./LineGraphComponent";
+
+const SeochoOkConsulting = ({ type, title }) => {
+  const [lineGraphInfo, setLineGraphInfo] = useState({
+    labels: [],
+    data: [],
+    totalRows: 0,
+  });
+
+  const [genderGraphInfo, setGenderGraphInfo] = useState({
+    totalRows: 0,
+    genderData: [],
+    ageData: [],
+  });
+
+  useEffect(() => {
+    const getLineGraphInfo = async () => {
+      const url = `${process.env.REACT_APP_SERVICE_API}/api/v1/stat/ok/${type}`;
+
+      try {
+        const response = await axios.get(url);
+
+        if (response.status === 200) {
+          const { totalRows, thisData } = response.data;
+
+          let labels = [];
+          let data = [];
+
+          for (let i = 0; i < thisData.length; i++) {
+            labels.push(parsingMonthDate(thisData[i].date));
+            data.push(thisData[i].cnt);
+          }
+
+          setLineGraphInfo({
+            labels: labels,
+            data: data,
+            totalRows: totalRows,
+          });
+        }
+      } catch (e) {
+        console.log(e);
+        alert(
+          `${
+            type === "online" ? "온라인" : "전화"
+          } 상담자 통계 조회 중, 오류가 발생하였습니다.`
+        );
+      }
+    };
+
+    const getBarGraphInfo = async () => {
+      const url = `${process.env.REACT_APP_SERVICE_API}/api/v1/stat/ok/${type}/gender`;
+
+      try {
+        const response = await axios.get(url);
+
+        if (response.status === 200) {
+          const { totalRows, genderData, ageData } = response.data;
+
+          let genderary = [];
+
+          for (let i = 0; i < genderData.length; i++) {
+            if (
+              genderData[i].gender === "M" ||
+              genderData[i].gender === "남성"
+            ) {
+              genderary.push({
+                gender: "m",
+                cnt: genderData[i].cnt,
+              });
+            } else if (
+              genderData[i].gender === "F" ||
+              genderData[i].gender === "여성"
+            ) {
+              genderary.push({
+                gender: "f",
+                cnt: genderData[i].cnt,
+              });
+            } else {
+              genderary.push({
+                gender: "u",
+                cnt: genderData[i].cnt,
+              });
+            }
+          }
+
+          let ageAry = [];
+          for (let i = 0; i < ageData.length; i++) {
+            ageAry.push({
+              age: ageData[i].age === "Unknown" ? "나이 미상" : ageData[i].age,
+              rate: calcRate(ageData[i].cnt, totalRows),
+            });
+          }
+
+          setGenderGraphInfo({
+            totalRows: totalRows,
+            genderData: genderary,
+            ageData: ageAry,
+          });
+        }
+      } catch (e) {
+        console.log(e);
+        alert(
+          `${
+            type === "online" ? "온라인" : "전화"
+          } 상담 통계 조회 중, 오류가 발생하였습니다.`
+        );
+      }
+    };
+
+    getLineGraphInfo();
+    getBarGraphInfo();
+  }, [type]);
+
   return (
     <>
       <div className="page-separator">
-        <div className="page-separator__text">서초OK생활 온라인 상담</div>
+        <div className="page-separator__text">{title}</div>
       </div>
       <div className="row card-group-row mb-lg-8pt">
-        <div className="col-md-6 card-group-row__col">
-          <div className="card card-group-row__card">
-            <div className="card-header p-0 nav">
-              <div className="row no-gutters flex" role="tablist">
-                <div className="col-auto">
-                  <div className="p-card-header d-flex align-items-center">
-                    <div className="h2 mb-0 mr-3">417</div>
-                    <div className="flex">
-                      <p className="mb-0">
-                        <strong>상담 수</strong>
-                      </p>
-                      <p className="text-50 mb-0 d-flex align-items-center">
-                        <small>이번주 + 240</small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card-body">
-              <div
-                id="legend"
-                className="chart-legend mt-0 mb-24pt justify-content-start"
-              ></div>
-              <div className="chart" style={{ height: "182px" }}>
-                <canvas
-                  id="earningsChart"
-                  className="chart-canvas js-update-chart-bar"
-                  data-chart-legend="#legend"
-                  data-chart-line-background-color="primary,gray"
-                  data-chart-prefix="$"
-                  data-chart-suffix="k"
-                >
-                  <span style={{ fontSize: "1rem" }}>
-                    <strong>Views</strong> area chart goes here.
-                  </span>
-                </canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6 card-group-row__col">
-          <div className="card card-group-row__card">
-            <div className="card-header p-0 nav">
-              <div className="row no-gutters flex" role="tablist">
-                <div className="col-auto">
-                  <div className="p-card-header d-flex align-items-center float-left">
-                    <div className="h2 mb-0 mr-3">103</div>
-                    <div className="flex">
-                      <p className="mb-0">
-                        <strong>남성 활동자</strong>
-                      </p>
-                      <p className="text-50 mb-0 d-flex align-items-center">
-                        <small>52%</small>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="p-card-header d-flex align-items-center float-left">
-                    <div className="h2 mb-0 mr-3">103</div>
-                    <div className="flex">
-                      <p className="mb-0">
-                        <strong>여성 활동자</strong>
-                      </p>
-                      <p className="text-50 mb-0 d-flex align-items-center">
-                        <small>48%</small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card-body d-flex flex-column">
-              <div className="d-flex flex flex-column align-items-center justify-content-center">
-                <ul className="list-unstyled list-skills w-100">
-                  <li className="mb-8pt">
-                    <div className="text-50 border-right">
-                      <small>~20대</small>
-                    </div>
-                    <div className="flex">
-                      <div className="progress" style={{ height: "4px" }}>
-                        <div
-                          className="progress-bar bg-primary"
-                          role="progressbar"
-                          style={{ width: "40%" }}
-                          aria-valuenow="40"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                    <div className="text-70">
-                      <small>20%</small>
-                    </div>
-                  </li>
-                  <li className="mb-8pt">
-                    <div className="text-50 border-right">
-                      <small>30대</small>
-                    </div>
-                    <div className="flex">
-                      <div className="progress" style={{ height: "4px" }}>
-                        <div
-                          className="progress-bar bg-primary"
-                          role="progressbar"
-                          style={{ width: "50%" }}
-                          aria-valuenow="50"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                    <div className="text-70">
-                      <small>40%</small>
-                    </div>
-                  </li>
-                  <li className="mb-8pt">
-                    <div className="text-50 border-right">
-                      <small>30대~</small>
-                    </div>
-                    <div className="flex">
-                      <div className="progress" style={{ height: "4px" }}>
-                        <div
-                          className="progress-bar bg-primary"
-                          role="progressbar"
-                          style={{ width: "60%" }}
-                          aria-valuenow="60"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                    <div className="text-70">
-                      <small>30%</small>
-                    </div>
-                  </li>
-                  <li className="mb-8pt">
-                    <div className="text-50 border-right">
-                      <small>50대~</small>
-                    </div>
-                    <div className="flex">
-                      <div className="progress" style={{ height: "4px" }}>
-                        <div
-                          className="progress-bar bg-primary"
-                          role="progressbar"
-                          style={{ width: "90%" }}
-                          aria-valuenow="90"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                    <div className="text-70">
-                      <small>10%</small>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <LineGraphComponent title="상담수" lineGraphInfo={lineGraphInfo} />
+        <HorizontalBarGraphComponent genderGraphInfo={genderGraphInfo} />
       </div>
     </>
   );
