ae83662cd0e960af3fe7daf413527bf6d5a9efc4
ChungJooEun <nina2439@naver.com>
2022-08-09 04:16:01 +0900

    OK 생활 자문단 > 온라인 상담 > 목록 조회 - 정렬 기능 구현

--- src/components/sc-ok/online-consultation-components/OnlineConsultationList.js
+++ src/components/sc-ok/online-consultation-components/OnlineConsultationList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import OnlineConsultationListItem from "./OnlineConsultationListItem";
 
-const style = { width: "48px" };
+const style = { width: "48px", cursor: "pointer" };
 const OnlineConsultationList = ({
   list,
   userName,
@@ -10,6 +10,7 @@ const OnlineConsultationList = ({
   count,
   expertName,
   dateString,
+  onChangeSortInfo,
 }) => {
   let no = (pageNumber - 1) * count + 1;
 
@@ -24,26 +25,29 @@ const OnlineConsultationList = ({
       <table className="table mb-0 thead-border-top-0 table-nowrap text-align-left">
         <thead>
           <tr>
-            <th style={style}>
+            <th style={style} onClick={() => onChangeSortInfo("idx")}>
               <span className="sort">No.</span>
             </th>
-            <th style={style}>
+            <th style={style} onClick={() => onChangeSortInfo("title")}>
               <span className="sort">제목</span>
             </th>
-            <th style={style}>
+            <th style={style} onClick={() => onChangeSortInfo("area")}>
               <span className="sort">상담분야</span>
             </th>
-            <th style={style}>
+            <th style={style} onClick={() => onChangeSortInfo("name")}>
               <span className="sort">성명</span>
             </th>
 
-            <th style={style}>
+            <th
+              style={style}
+              onClick={() => onChangeSortInfo("consultationDate")}
+            >
               <span className="sort">일정</span>
             </th>
-            <th style={style}>
+            <th style={style} onClick={() => onChangeSortInfo("expertName")}>
               <span className="sort">전문가</span>
             </th>
-            <th style={style}>
+            <th style={style} onClick={() => onChangeSortInfo("status")}>
               <span className="sort">답변상태</span>
             </th>
             {/*
